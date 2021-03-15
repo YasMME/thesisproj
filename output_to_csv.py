7a30683253b8140afb620efea5093494f2f886a6
Yasmeen <yasMME@gmail.com>
2021-03-15 11:05:18 +0100

    WIP planning report

--- output_to_csv.py
+++ output_to_csv.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import json
+import sys
 
-FILE_NAME = 'train.json'
 
-with open(FILE_NAME, 'r') as statfile:
+INPUT_FILE_NAME = sys.argv[0]
+OUTPUT_FILE_NAME = sys.argv[1]
+
+with open(INPUT_FILE_NAME, 'r') as statfile:
     data = statfile.read()
 
 obj = json.loads(data)
 columns = [x for x in obj['metric_names']]
 df = pd.DataFrame(obj['stats'], columns=columns)
 
-CSV_NAME = 'train.csv'
-df.to_csv(CSV_NAME)
+df.to_csv(OUTPUT_FILE_NAME)
